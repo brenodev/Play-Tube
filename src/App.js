7a70d4c39ab4d5e776e2a3c24e04e3ca904cdfd1
Breno Guilherme de Souza Lopes <bg.souza.lopes@hotmail.com>
2019-07-02 12:08:35 -0300

    criado componentes searchbar videolist e videoplayer

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+// CUSTOM COMPONENTS
+import SearchBar from './components/SearchBar'
+import VideoList from './components/VideoList'
+import VideoPlayer from './components/VideoPlayer'
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <SearchBar />
+      <VideoList />
+      <VideoPlayer />
+    </div>
+  );
+}
+
+export default App;
