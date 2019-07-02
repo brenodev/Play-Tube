1946947a26bbd5dc0fe56a9729a3462b23044f7a
Breno Guilherme de Souza Lopes <bg.souza.lopes@hotmail.com>
2019-07-02 14:12:24 -0300

    criado funcao bara realizar pesquisa

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,11 +2,25 @@ import React, { Component } from "react";
 import { Segment, Input } from "semantic-ui-react";
 
 class SearchBar extends Component {
+
+//pesquisa o termo e retorna o resultado
+researchTerm = e => {
+  const term = e.target.value
+  if (e.keyCode === 13) {
+    console.log(term)
+  }
+}
+
   render() {
     return (
       <div className="search-bar">
         <Segment stacked>
-          <Input icon="search" size="large" placeholder="Search..." />
+          <Input 
+            icon="search" 
+            size="large" 
+            placeholder="Search ..." 
+            onKeyDown={(e) => this.researchTerm(e)}
+          />
         </Segment>
       </div>
     );
