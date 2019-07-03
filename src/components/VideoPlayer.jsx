a523a2c84bafee8b7cdf2a1aee9328033a44f64d
Breno Guilherme de Souza Lopes <bg.souza.lopes@hotmail.com>
2019-07-03 17:16:27 -0300

    reproduzindo o video

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,11 +1,41 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { Advertisement, Embed } from 'semantic-ui-react'
 
 const VideoPlayer = props => {
   return (
-    <div className='video-player'> 
-      <h1>VideoPlayer</h1>
-    </div>
+  <div className='video-player'> 
+{/* mostra algo enquanto não tem video  */}
+    {
+      !props.video.id && (
+        <Advertisement 
+        style={{'height':'555px' }}
+        unit='top banner'
+        test='Escolha um video para reproduzir'
+      />
+      )
+    }
+{/* reproduz o video clicado */}
+    {
+      props.video.id && (
+      <div>
+        <Embed 
+          id={props.video.id.videoId} 
+          source='youtube' 
+          placeholder='https://i.pining.com/originals/a2/81/38/a28138c18f0e24e9bab084aa97770be.jpg'
+        /> 
+        <p>{props.video.snippet.title}</p>
+        <p>{props.video.snippet.description}</p>
+      </div>
+      )
+    }   
+  </div>
   )
 }
+const mapStateToProps = (state) => {
+  return {
+    video: state.playVideoReducer.video
+  }
+}
 
-export default VideoPlayer
+export default connect(mapStateToProps, null)(VideoPlayer)
