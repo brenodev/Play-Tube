7999b345a9eebb119d7c11e520c73bfe8a280b81
Breno Guilherme de Souza Lopes <bg.souza.lopes@hotmail.com>
2019-07-03 16:21:30 -0300

    criado reducer para reproduzir o video

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,10 +1,13 @@
 // combine redux 
 import { combineReducers } from 'redux'
 
+// REDUCERS
 import searchVideoReducer from './searchVideoReducer'
+import playVideoReducer from './playVideoReducer'
 
 const rootReducer = combineReducers({
-  searchVideoReducer
+  searchVideoReducer,
+  playVideoReducer
 })
 
 export default rootReducer
