4a359dc6fdae06a7505cecfea0514e792e664f13
Breno Guilherme de Souza Lopes <bg.souza.lopes@hotmail.com>
2019-07-03 13:13:16 -0300

    configurado extension do redux e iniciado configuracao do componente videolist

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,13 +1,51 @@
-import React from 'react'
+import React from "react";
+import { List, Image } from "semantic-ui-react";
 
 const VideoList = props => {
   return (
     <div className="video-list">
-      <h1>
-        VideoList
-      </h1>
+      <List animated verticalAlign="middle">
+        <List.Item>
+          <Image avatar src="" />
+          <List.Content>
+            <List.Header>Título do Vídeo</List.Header>
+          </List.Content>
+        </List.Item>
+      </List>
+      <List animated verticalAlign="middle">
+        <List.Item>
+          <Image avatar src="" />
+          <List.Content>
+            <List.Header>Título do Vídeo</List.Header>
+          </List.Content>
+        </List.Item>
+      </List>
+      <List animated verticalAlign="middle">
+        <List.Item>
+          <Image avatar src="" />
+          <List.Content>
+            <List.Header>Título do Vídeo</List.Header>
+          </List.Content>
+        </List.Item>
+      </List>
+      <List animated verticalAlign="middle">
+        <List.Item>
+          <Image avatar src="" />
+          <List.Content>
+            <List.Header>Título do Vídeo</List.Header>
+          </List.Content>
+        </List.Item>
+      </List>
+      <List animated verticalAlign="middle">
+        <List.Item>
+          <Image avatar src="" />
+          <List.Content>
+            <List.Header>Título do Vídeo</List.Header>
+          </List.Content>
+        </List.Item>
+      </List>
     </div>
-  )
-}
+  );
+};
 
-export default VideoList
+export default VideoList;
--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,7 @@ import logger from 'redux-logger'
 // CUSTOM REDUCERS
 import reducers from './reducers/index'
 
-const composeEnhancers = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const middlewares = [ thunk, logger]
 
 const store = createStore(reducers, composeEnhancers(
