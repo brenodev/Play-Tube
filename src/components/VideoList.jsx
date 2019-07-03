ccb27eb245c56fbca7633409d665c1522cb080c8
Breno Guilherme de Souza Lopes <bg.souza.lopes@hotmail.com>
2019-07-03 13:55:20 -0300

    conectado componente videoList com store

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { List, Image } from "semantic-ui-react";
+import { connect } from 'react-redux'
 
 const VideoList = props => {
   return (
@@ -12,40 +13,17 @@ const VideoList = props => {
           </List.Content>
         </List.Item>
       </List>
-      <List animated verticalAlign="middle">
-        <List.Item>
-          <Image avatar src="" />
-          <List.Content>
-            <List.Header>Título do Vídeo</List.Header>
-          </List.Content>
-        </List.Item>
-      </List>
-      <List animated verticalAlign="middle">
-        <List.Item>
-          <Image avatar src="" />
-          <List.Content>
-            <List.Header>Título do Vídeo</List.Header>
-          </List.Content>
-        </List.Item>
-      </List>
-      <List animated verticalAlign="middle">
-        <List.Item>
-          <Image avatar src="" />
-          <List.Content>
-            <List.Header>Título do Vídeo</List.Header>
-          </List.Content>
-        </List.Item>
-      </List>
-      <List animated verticalAlign="middle">
-        <List.Item>
-          <Image avatar src="" />
-          <List.Content>
-            <List.Header>Título do Vídeo</List.Header>
-          </List.Content>
-        </List.Item>
-      </List>
+    <p>{JSON.stringify(props)}</p>
     </div>
   );
 };
 
-export default VideoList;
+const mapStateToProps = (state) => {
+  return {
+    video: state.searchVideoReducer.video,
+    loading: state.searchVideoReducer.loading,
+    error: state.searchVideoReducer.error
+  }
+}
+
+export default connect(mapStateToProps, null)(VideoList)
